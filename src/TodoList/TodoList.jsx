4bc0ceca6b39a78290472b9787aec2d2ea54665b
erika11111 <104774933+erika11111@users.noreply.github.com>
2023-11-12 15:52:42 +0100

    Sorting item list based on completed status

--- src/TodoList/TodoList.jsx
+++ src/TodoList/TodoList.jsx
@@ -2,11 +2,17 @@ import styles from "./TodoList.module.css";
 import TodoItem from "../TodoItem/TodoItem";
 // eslint-disable-next-line react/prop-types
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    //slice method created a copy of an array
+    // eslint-disable-next-line react/prop-types
+    .slice()
+    //completed items are on one side and incompleted on other - and sorted in an ascending order (a-b)
+    .sort((a, b) => Number(a.completed) - Number(b.completed));
   return (
     <div className={styles.list}>
       {
         // eslint-disable-next-line react/prop-types
-        todos.map((item) => (
+        sortedTodos.map((item) => (
           <TodoItem
             key={item.name}
             item={item}
